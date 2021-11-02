49567281a9f0bf7105c5b46ab67f36712a7f47b1
Stephani0106 <stephani182@hotmail.com>
2021-11-01 21:24:55 -0300

    Add css style

--- src/components/pokeCards.jsx
+++ src/components/pokeCards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card'; 
 import { CardContent, CardMedia, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+// import SyncIcon from '@mui/icons-material/Sync';
 import styles from '../styles/css/pokeCards.module.css';
 
 const useStyles = makeStyles({
@@ -14,6 +15,18 @@ const useStyles = makeStyles({
         boxShadow: '0px 4px 9px rgb(0 0 0 / 40%);',
         backgroundImage: 'linear-gradient(#619fbd, #c5ecdf)',
     },
+    title: {
+        fontFamily: 'Pokemon',
+        fontSize: '20px',
+        color: '#3167af',
+        textAlign: 'center',
+        textShadow: '2px 2px #1f3365',
+    }, 
+    type: {
+        fontFamily: 'Pokemon',
+        fontSize: '14px',
+        color: '#5e5741'
+    }
 });
 
 export default function PokeCards({ image, name, type }) {
@@ -22,11 +35,11 @@ export default function PokeCards({ image, name, type }) {
     return (
         <Card className={stylesUI.root}>
             <CardContent>
-                <Typography className={styles.title}>
+                <Typography className={stylesUI.title}>
                     {name}
                 </Typography>
-                <Typography className={styles.type}>
-                    {type}
+                <Typography className={stylesUI.type}>
+                    Tipo: {type}
                 </Typography>
 
                 <CardMedia 
